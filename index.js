f08fe6b3aab839becf45e87f4a58b92b7fb2aaaa
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2022-12-13 23:48:21 -0500

    added cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
