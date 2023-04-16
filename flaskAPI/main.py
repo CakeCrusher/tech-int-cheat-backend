b9a348760f6c325fe77d78475eadb85013003866
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2023-04-16 13:37:50 -0400

    update

---
+++ flaskAPI/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import openai
+from dotenv import load_dotenv
+import os
+# prevent blocking from cors policy
+from flask_cors import CORS
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+app = Flask(__name__)
+CORS(app)
+
+openai.api_key = API_KEY
+
+# create a post function tjat take in an array of messages
+@app.route('/infer_response', methods=['POST'])
+def example():
+    if request.method == 'POST':
+        # Retrieve the data from the request body
+        chatSlice = request.get_json()
+        # transform the chatSlice roles to upper case and then from "YOU"  to "user" and any other role to "assistant"
+        for i in range(len(chatSlice)):
+            chatSlice[i]['role'] = chatSlice[i]['role'].upper()
+            if chatSlice[i]['role'] == "YOU":
+                chatSlice[i]['role'] = "user"
+            else:
+                chatSlice[i]['role'] = "assistant"
+
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=chatSlice
+        )
+        generatedText = res.choices[0].message.content
+
+        # Return a JSON response
+        return jsonify({'response': generatedText})
+
+if __name__ == '__main__':
+    app.run(debug=True)
